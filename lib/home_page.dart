3c404ade96429e34215f492262258e8a088027bb
rafaxzx <rafa_xzx@hotmail.com>
2020-06-14 08:20:21 -0300

    New branch to add new feature, Timer Light ON

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:torch_compat/torch_compat.dart';
 import 'package:shake/shake.dart';
@@ -8,16 +10,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  static bool _isLightOn;
-  static bool _shakeOn;
-  static double _gravity = 2.5;
+  //Variables
+  bool _isLightOn;
+  bool _shakeOn;
+  bool _isTimerOn;
+  int _secondsPreset;
+  int _secondsTimer;
+  final double _gravity = 2.5;
+
   ShakeDetector detector;
+  Timer _timer;
 
   @override
   void initState() {
     super.initState();
     _isLightOn = false;
     _shakeOn = true;
+    _isTimerOn = false;
+    _secondsPreset = 15;
+    _secondsTimer = _secondsPreset;
+    //Function that control the light
     TorchCompat.turnOff();
     detector = ShakeDetector.autoStart(
         shakeThresholdGravity: _gravity,
@@ -30,7 +42,6 @@ class _HomePageState extends State<HomePage> {
   void dispose() {
     super.dispose();
     detector.stopListening();
-    print('>>>>>>>>>>>>>>>>> DETECTOR PAROU DE OUVIR <<<<<<<<<<<<<<<<<<<<');
   }
 
   @override
@@ -61,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                   });
                 }),
             IconButton(
-                padding: EdgeInsets.all(20.0),
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
                 iconSize: 80.0,
                 icon: Icon(
                   Icons.vibration,
@@ -69,19 +80,81 @@ class _HomePageState extends State<HomePage> {
                   color: _shakeOn ? Colors.yellow : Colors.black,
                 ),
                 onPressed: () {
-                  _shakeOn = !_shakeOn;
-                  _shakeOn
-                      ? detector.startListening()
-                      : detector.stopListening();
-                  setState(() {});
+                  setState(() {
+                    _shakeOn = !_shakeOn;
+                    _shakeOn ? detector.startListening() : detector.stopListening();
+                  });
+                }),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Text(
+                "Shake mode: ${_shakeOn ? 'ON' : 'OFF'}",
+                style: TextStyle(
+                    color: _shakeOn ? Colors.yellow : Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            IconButton(
+                padding: EdgeInsets.only(bottom: 0, top: 20),
+                iconSize: 80.0,
+                icon: Icon(
+                  Icons.watch_later,
+                  size: 80.0,
+                  color: _isTimerOn ? Colors.yellow : Colors.black,
+                ),
+                onPressed: () {
+                  _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+                    if (_secondsTimer > 0) {
+                      setState(() {
+                        _isTimerOn = true;
+                        if (_secondsPreset == _secondsTimer) _turnOnOffTorch(_isTimerOn);
+                        _secondsTimer -= 1;
+                      });
+                    } else {
+                      _timer.cancel();
+                      setState(() {
+                        _isTimerOn = false;
+                        if (_secondsTimer == 0) _turnOnOffTorch(_isTimerOn);
+                      });
+                    }
+                  });
                 }),
             Text(
-              "Shake MODE: ${_shakeOn ? 'ligado' : 'desligado'}",
+              _isTimerOn ? '$_secondsTimer seconds remaining' : 'Timer',
               style: TextStyle(
-                  color: _shakeOn ? Colors.yellow : Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
+                fontSize: 20.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            DropdownButton(
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              value: _secondsPreset,
+              onChanged: (value) {
+                setState(() {
+                  _secondsPreset = value;
+                });
+              },
+              items: <DropdownMenuItem>[
+                DropdownMenuItem(
+                  child: Text('15 seconds'),
+                  value: 15,
+                ),
+                DropdownMenuItem(
+                  child: Text('30 seconds'),
+                  value: 30,
+                ),
+                DropdownMenuItem(
+                  child: Text('1 minute'),
+                  value: 60,
+                ),
+              ],
+            )
           ],
         ),
       ),
@@ -93,6 +166,16 @@ class _HomePageState extends State<HomePage> {
       _isLightOn ? TorchCompat.turnOff() : TorchCompat.turnOn();
       _isLightOn = !_isLightOn;
     });
+  }
+
+  void _turnOnOffTorch(bool command) {
+    if (command) {
+      TorchCompat.turnOn();
+      _isLightOn = true;
+    } else {
+      TorchCompat.turnOff();
+      _isLightOn = false;
+    }
   }
 
   Color _getColorState() {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_Lanterna_by_Rafael/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -6,10 +7,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Lanterna Simples by Rafael',
+      title: 'Lanterna Flutter',
       theme: ThemeData(
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
