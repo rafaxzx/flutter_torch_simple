76cab8103fb63fa6dfa88878e111507c1de2a254
rafaxzx <rafa_xzx@hotmail.com>
2020-06-16 18:02:41 -0300

    Add disable timer function to lamp button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -25,18 +25,13 @@ class _HomePageState extends State<HomePage> {
 
   //AdMob Configuration
 //  static const String testDevice = null;
-//  static const String testDevice = 'MObile_id';
-  static const String testDevice = '40AE2B4B64E7CF94D78A4962C114B9F4';
+  static const String testDevice = 'MObile_id';
+//  static const String testDevice = '40AE2B4B64E7CF94D78A4962C114B9F4';
 
   //Target info settings
   static const MobileAdTargetingInfo _targetingInfo = MobileAdTargetingInfo(
       childDirected: false,
-      keywords: <String>[
-        'lanterna',
-        'iluminação',
-        'luz emergencia',
-        'luz emergência'
-      ],
+      keywords: keywords,
       testDevices: testDevice != null ? <String>[testDevice] : null,
       contentUrl: 'https://github.com/rafaxzx',
       nonPersonalizedAds: true);
@@ -93,115 +88,145 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.black,
         centerTitle: true,
-        title: Text("Lanterna ${_isLightOn ? 'ligada' : 'desligada'}"),
+        title: Text("Lanterna Flutter ${_isLightOn ? 'ligada' : 'desligada'}"),
       ),
       backgroundColor: _getColorState(),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-                padding: EdgeInsets.all(20.0),
-                iconSize: 80.0,
-                icon: Icon(
-                  Icons.highlight,
-                  size: 80.0,
-                  color: _isLightOn ? Colors.yellow : Colors.black,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 120),
+                child: IconButton(
+                    padding: EdgeInsets.all(20.0),
+                    iconSize: 80.0,
+                    icon: Icon(
+                      Icons.highlight,
+                      size: 80.0,
+                      color: _isLightOn ? Colors.yellow : Colors.black,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _changeTorch();
+                        if (_timer != null) {
+                          _timer.cancel();
+                          _secondsTimer = 0;
+                          _isTimerOn = false;
+                        }
+                      });
+                    }),
+              ),
+              Container(
+                child: IconButton(
+                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    iconSize: 80.0,
+                    icon: Icon(
+                      Icons.vibration,
+                      size: 80.0,
+                      color: _shakeOn ? Colors.yellow : Colors.black,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _shakeOn = !_shakeOn;
+                        _shakeOn
+                            ? detector.startListening()
+                            : detector.stopListening();
+                      });
+                    }),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+//                    "Modo agitar: ${_shakeOn ? 'ON' : 'OFF'}",
+                    "Modo agitar",
+                    style: TextStyle(
+                        color: _shakeOn ? Colors.yellow : Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                onPressed: () {
-                  setState(() {
-                    _changeTorch();
-                  });
-                }),
-            IconButton(
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                iconSize: 80.0,
-                icon: Icon(
-                  Icons.vibration,
-                  size: 80.0,
-                  color: _shakeOn ? Colors.yellow : Colors.black,
-                ),
-                onPressed: () {
-                  setState(() {
-                    _shakeOn = !_shakeOn;
-                    _shakeOn
-                        ? detector.startListening()
-                        : detector.stopListening();
-                  });
-                }),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Text(
-                "Shake mode: ${_shakeOn ? 'ON' : 'OFF'}",
-                style: TextStyle(
-                    color: _shakeOn ? Colors.yellow : Colors.black,
+              ),
+              Container(
+                child: IconButton(
+                    padding: EdgeInsets.only(bottom: 0, top: 20),
+                    iconSize: 80.0,
+                    icon: Icon(
+                      Icons.watch_later,
+                      size: 80.0,
+                      color: _isTimerOn ? Colors.yellow : Colors.black,
+                    ),
+                    onPressed: () {
+                      _secondsTimer = _secondsPreset;
+                      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+                        if (_secondsTimer > 0) {
+                          setState(() {
+                            if (_shakeOn) {
+                              _shakeOn = false;
+                              detector.stopListening();
+                            }
+                            _isTimerOn = true;
+                            if (_secondsPreset == _secondsTimer)
+                              _turnOnOffTorch(_isTimerOn);
+                            _secondsTimer -= 1;
+                          });
+                        } else {
+                          setState(() {
+                            timer.cancel();
+                            _timer.cancel();
+                            _isTimerOn = false;
+                            if (_secondsTimer == 0) _turnOnOffTorch(_isTimerOn);
+                            _timer = null;
+                          });
+                        }
+                      });
+                    }),
+              ),
+              Container(
+                child: Text(
+                  _isTimerOn
+                      ? '$_secondsTimer segundos restantes'
+                      : 'Temporizador',
+                  style: TextStyle(
                     fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
-            IconButton(
-                padding: EdgeInsets.only(bottom: 0, top: 20),
-                iconSize: 80.0,
-                icon: Icon(
-                  Icons.watch_later,
-                  size: 80.0,
-                  color: _isTimerOn ? Colors.yellow : Colors.black,
+              Container(
+                child: DropdownButton(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  value: _secondsPreset,
+                  onChanged: (value) {
+                    setState(() {
+                      _secondsPreset = value;
+                    });
+                  },
+                  items: <DropdownMenuItem>[
+                    DropdownMenuItem(
+                      child: Text('15 segundos'),
+                      value: 15,
+                    ),
+                    DropdownMenuItem(
+                      child: Text('30 segundos'),
+                      value: 30,
+                    ),
+                    DropdownMenuItem(
+                      child: Text('1 minuto'),
+                      value: 60,
+                    ),
+                  ],
                 ),
-                onPressed: () {
-                  _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-                    if (_secondsTimer > 0) {
-                      setState(() {
-                        _isTimerOn = true;
-                        if (_secondsPreset == _secondsTimer)
-                          _turnOnOffTorch(_isTimerOn);
-                        _secondsTimer -= 1;
-                      });
-                    } else {
-                      _timer.cancel();
-                      setState(() {
-                        _isTimerOn = false;
-                        if (_secondsTimer == 0) _turnOnOffTorch(_isTimerOn);
-                      });
-                    }
-                  });
-                }),
-            Text(
-              _isTimerOn ? '$_secondsTimer seconds remaining' : 'Timer',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
               ),
-            ),
-            DropdownButton(
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-              value: _secondsPreset,
-              onChanged: (value) {
-                setState(() {
-                  _secondsPreset = value;
-                });
-              },
-              items: <DropdownMenuItem>[
-                DropdownMenuItem(
-                  child: Text('15 seconds'),
-                  value: 15,
-                ),
-                DropdownMenuItem(
-                  child: Text('30 seconds'),
-                  value: 30,
-                ),
-                DropdownMenuItem(
-                  child: Text('1 minute'),
-                  value: 60,
-                ),
-              ],
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -231,4 +256,32 @@ class _HomePageState extends State<HomePage> {
       return Colors.grey;
     }
   }
+
+  static const keywords = [
+    'Insurance',
+    'Loans',
+    'Mortgage',
+    'Attorney',
+    'Credit',
+    'Lawyer',
+    'Donate',
+    'Degree',
+    'Hosting',
+    'Claim',
+    'Conference Call',
+    'Trading',
+    'Software',
+    'Recovery',
+    'Transfer',
+    'Gas/Electicity',
+    'Classes'
+  ];
+//  static const keywords2 = [
+//    'lanterna',
+//    'iluminação',
+//    'luz emergencia',
+//    'luz emergência',
+//    'Insurance',
+//    'Loans'
+//  ];
 }
